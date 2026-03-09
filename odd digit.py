f5113050d381563676dafb66d635cb61883baac1
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-03-10 01:23:32 +0530

    ok

--- odd digit.py
+++ odd digit.py
@@ -1,17 +1,9 @@
 # find some of odd digits in a given user number
-# num  = input("Enter your number: ")
-# new_no = 0
-# for digit in num:
-#     n = int(digit)
-#     if n%2!=0:
-#         new_no += n
-# print(new_no) 
-
-a = int(input("Enter your no: "))
-rem = 0
-num = 0
-while (a>0):
-    a//10 = num
-    a%2 != rem
-    print(num+rem)
+num  = input("Enter your number: ")
+new_no = 0
+for digit in num:
+    n = int(digit)
+    if n%2!=0:
+        new_no += n
+print(new_no) 
 
