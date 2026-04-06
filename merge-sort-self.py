d1d2a3384091cc41ceb5b301a1e2b7ce387e7af7
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-04-06 13:15:25 +0530

    ik

---
+++ merge-sort-self.py
@@ -0,0 +1,13 @@
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    
+    mid = len(arr)//2
+    left_arr = merge_sort(arr[:mid])
+    right_arr = merge_sort(arr[mid:])
+    
+    return merge(left_arr,right_arr)
+
+def merge(left_arr,right_arr):
+    
+    
