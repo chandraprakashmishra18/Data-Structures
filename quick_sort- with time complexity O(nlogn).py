96ca49e29dccd2612a6f84d92bf88e62afc3fa12
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-04-15 12:22:06 +0530

    ikik

--- quick_sort- with time complexity O(nlogn).py
+++ quick_sort- with time complexity O(nlogn).py
@@ -1,14 +1,14 @@
-def quick_sort(arr , low , high):
+def quick_sort(arr ,low ,high):
     if low < high:
-        pi = partition(arr , low , high)
-        quick_sort(arr ,  low ,pi-1)
-        quick_sort(arr , pi+1 , high)
+        pi = partition(arr ,low ,high)
+        quick_sort(arr ,low ,pi-1)
+        quick_sort(arr ,pi+1 ,high)
     
-def partition(arr ,  low , high ):
+def partition(arr ,low ,high ):
         pivot = arr[high]
         i = low - 1
         
-        for j in range(low , high):
+        for j in range(low ,high):
             if arr[j] < pivot:
                 i += 1
                 arr[i] , arr[j] = arr[j] , arr[i]
