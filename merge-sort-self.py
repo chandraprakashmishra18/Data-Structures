abe90101502e5cbbc2eae23b7848e2b4362ee86c
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-04-21 23:05:49 +0530

    okkok

--- merge-sort-self.py
+++ merge-sort-self.py
@@ -3,8 +3,8 @@ def merge_sort(arr):
         return arr
     
     mid = len(arr)//2
-    left = merge(arr[0:mid])
-    right = merge(arr[mid:])
+    left = merge_sort(arr[0:mid])
+    right = merge_sort(arr[mid:])
     return merge(left,right)
     
 def merge(left,right):
@@ -13,8 +13,10 @@ def merge(left,right):
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
             result.append(left[i])
+            i+=1
         else:
             result.append(right[j])
+            j+=1
     
     result.extend(left[i:])
     result.extend(right[j:])
--- mergesort.py
+++ mergesort.py
@@ -1,35 +1,26 @@
 def merge_sort(arr):
-    if len(arr) <= 1:
+    if len(arr)<=1:
         return arr
-
-    # Divide
-    mid = len(arr) // 2
+    
+    mid = len(arr)//2
     left = merge_sort(arr[:mid])
     right = merge_sort(arr[mid:])
-
-    # Merge
-    return merge(left, right)
-
-
-def merge(left, right):
+    return merge(left,right)
+def merge(left,right):
     result = []
-    i = j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
+    i=j=0
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
             result.append(left[i])
-            i += 1
+            i+=1
         else:
             result.append(right[j])
-            j += 1
-
-    # Add remaining elements
+            j+=1
+        
     result.extend(left[i:])
     result.extend(right[j:])
-
     return result
 
-
-# Example
-arr = [38, 27, 43, 3, 9, 82, 10]
-print(merge_sort(arr))
+arr = [4,5,9,8,6,3,7]
+ray = merge_sort(arr)
+print(ray)
