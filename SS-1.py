2a5c9f52fce8482d80dec1ca1520567342e7ffe4
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-04-30 23:35:25 +0530

    asdfghjkl;

--- SS-1.py
+++ SS-1.py
@@ -5,7 +5,7 @@ def selection_sort(arr):
         for j in range(i+1,n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
-        arr[i] , min_idx = min_idx , arr[i]
+        arr[i] , arr[min_idx] = arr[min_idx] , arr[i]
     
     return arr
 
